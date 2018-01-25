65ade030fce1ac596d2eb8e60a46206811d752ab
mianto <siddhantshaw97@gmail.com>
2018-01-25 22:29:22 +0530

    v0.1

--- index.js
+++ index.js
@@ -11,16 +11,35 @@ const yellowBlock = document.querySelector('#yellow');
 const display = document.querySelector('#display');
 const start = document.querySelector('#start');
 const strict = document.querySelector('#strict');
-
+const strictLight = document.querySelector('#strict-light');
 const checkBox = document.querySelector(`.switch input[type="checkbox"]`);
+
+
+
+
+let isRedElementClicked = false;
+let isBlueElementClicked = false;
+let isYellowElementClicked = false;
+let isGreenElementClicked = false;
+let strictClicked = false;
+
+
 
 
 function handleCheck() {
   if(checkBox.checked) {
+    setTimeout(() => {
+      display.classList.add('blink');
+    }, 1000);
+    display.classList.remove('blink');
     startGame();
   } else {
-    stopGame();
+    display.innerHTML = "";
   }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
@@ -34,69 +53,214 @@ function startGame() {
   display.innerHTML = `<span>- -</span>`;
 }
 
+
 function stopGame() {
-  display.innerHTML = "";
+  const highestTimeoutId = setTimeout(";");
+  for (let i = 0 ; i < highestTimeoutId ; i++) {
+    clearTimeout(i);
+  }
+  display.innerHTML = "!!";
 }
 
 
+// generates Random Number between 0 and max
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
 
-function gameOn() {
-  if (checkBox.checked) {
-    setTimeout(() => {
-      display.classList.add('blink');
-    }, 1000);
-    display.classList.remove('blink');
-    /*
-      0-> red
-      1->blue
-      2->yellow
-      3-> green
-    */
-    for (let i = 1;i < 2; i++) {
-      let randomArray = [];
-      for (let j = 0; j <= i; j++) {
-        randomArray.push(getRandomInt(5));
-      }
-      console.log(randomArray);
-      randomArray.forEach(x => {
-        if (x == "0") {
-          gameBlocks(redBlock, audioRed, i);
-        } else if (x == '1') {
-          gameBlocks(blueBlock, audioBlue, i);
-        } else if ( x == '2') {
-          gameBlocks(yellowBlock, audioYellow, i);
-        } else {
-          gameBlocks(greenBlock, audioGreen, i);
-        }
-      });
+async function gameOn() {
+  /*
+    0-> red
+    1->blue
+    2->yellow
+    3-> green
+  */
+  if ( !checkBox.checked ) {
+    return;
+  }
+  let timePeriod = 1000;
+  if ( !strictClicked ) {
+    timePeriod = 500;
+  }
+  let j = 1
+  for (let i = 0;i < 5; i++) {
 
+
+    let randomArray = [];
+    for (let k = 0; k <= i; k++) {
+      randomArray.push(getRandomInt(4));
     }
+    console.log(randomArray);
+
+
+    for (let x = 0; x < randomArray.length; x++) {
+
+      let t = j * timePeriod;
+      j++;
+      if (randomArray[x] == "0") {
+        setTimeout (() => {
+          makeRedActive(i + 1);
+          redBlock.addEventListener('click', checkRedPressed);
+          waitRedChecking();
+        }, t);
+
+      } else if (randomArray[x] == '1') {
+        setTimeout(() => {
+            makeBlueActive(i + 1);
+            blueBlock.addEventListener('click', checkBluePressed);
+            waitBlueChecking();
+          }, t);
+
+
+      } else if ( randomArray[x] == '2') {
+          setTimeout(() => {
+            makeYellowActive(i + 1);
+            yellowBlock.addEventListener('click', checkYellowPressed);
+            waitYellowChecking();
+          }, t);
+
+      } else if ( randomArray[x] == '3') {
+
+          setTimeout(() => {
+            makeGreenActive(i + 1);
+            greenBlock.addEventListener('click', checkGreenPressed);
+            waitGreenChecking();
+          }, t);
+
+      }
+    }
+    await sleep(5000);
   }
 }
 
+function makeRedActive(i) {
+    if (i < 10) {
+      display.innerHTML = `0${i}`;
+    } else {
+      display.innerHTML = `${i}`;
+    }
+    redBlock.style.backgroundColor = "rgba(255, 0, 0, 1)";
+    playAudio(audioRed);
+    let colorChange = setTimeout(() => {
+      redBlock.style.backgroundColor = "rgba(160, 0, 0, 1)";
+    }, 1000);
+}
 
-function gameBlocks (colorBlock, audioColor, i) {
+function makeBlueActive(i) {
+    if (i < 10) {
+      display.innerHTML = `0${i}`;
+    } else {
+      display.innerHTML = `${i}`;
+    }
+    blueBlock.style.backgroundColor = "rgba(0, 0, 255, 1)";
+    playAudio(audioBlue);
+    let colorChange = setTimeout(() => {
+      blueBlock.style.backgroundColor = "rgba(0, 0, 140, 1)";
+    }, 1000);
+}
 
-  setTimeout(() => {
-    colorBlock.classList.add('class-blink');
-    playAudio(audioColor);
+function makeGreenActive(i) {
+    if (i < 10) {
+      display.innerHTML = `0${i}`;
+    } else {
+      display.innerHTML = `${i}`;
+    }
+    greenBlock.style.backgroundColor = "rgba(0, 255, 0, 1)";
+    playAudio(audioGreen);
+    let colorChange = setTimeout(() => {
+      greenBlock.style.backgroundColor = "rgba(0, 160, 0, 1)";
+    }, 1000);
+}
+
+function makeYellowActive(i) {
+    if (i < 10) {
+      display.innerHTML = `0${i}`;
+    } else {
+      display.innerHTML = `${i}`;
+    }
+    yellowBlock.style.backgroundColor = "rgba(255, 255, 0, 1)";
+    playAudio(audioYellow);
+    let colorChange = setTimeout(() => {
+      yellowBlock.style.backgroundColor = "rgba(200, 180, 0, 1)";
+    }, 1000);
+}
+
+
+// check if the block is pressed or not within 1000ms
+function checkRedPressed() {
+  isRedElementClicked = true;
+}
+
+function checkBluePressed() {
+  isBlueElementClicked = true;
+}
+
+function checkGreenPressed() {
+  isGreenElementClicked = true;
+}
+
+function checkYellowPressed() {
+  isYellowElementClicked = true;
+}
+
+function waitRedChecking()  {
+  setTimeout( () => {
+    if(isRedElementClicked && !isBlueElementClicked && !isGreenElementClicked && !isYellowElementClicked) {
+      console.log("clicked");
+      isRedElementClicked = false;
+    } else {
+      stopGame();
+    }
   }, 2000);
-  colorBlock.classList.remove('class-blink');
-  if (i < 10) {
-    display.innerHTML = `0${i}`;
-  } else {
-    display.innerHTML = `${i}`;
-  }
-  // console.log(colorBlock);
+}
 
+function waitGreenChecking()  {
+  setTimeout( () => {
+    if(isGreenElementClicked && !isBlueElementClicked && !isRedElementClicked && !isRedElementClicked) {
+      console.log("clicked");
+      isGreenElementClicked = false;
+    } else {
+      stopGame();
+    }
+  }, 2000);
+}
 
+function waitBlueChecking()  {
+  setTimeout( () => {
+    if(isBlueElementClicked && !isGreenElementClicked && !isRedElementClicked && !isYellowElementClicked) {
+      console.log("clicked");
+      isBlueElementClicked = false;
+    } else {
+      stopGame();
+    }
+  }, 2000);
+}
 
+function waitYellowChecking()  {
+  setTimeout( () => {
+    if(isYellowElementClicked && !isRedElementClicked && !isBlueElementClicked && !isGreenElementClicked) {
+      console.log("clicked");
+      isYellowElementClicked = false;
+    } else {
+      stopGame();
+    }
+  }, 2000);
 }
 
 
+
+function strictMode() {
+  if (!strictClicked && checkBox.checked) {
+    strictLight.style.backgroundColor="green";
+    strictClicked = !strictClicked;
+  } else {
+    strictLight.style.backgroundColor="lightgray";
+    strictClicked = !strictClicked;
+  }
+}
+
+/* event Listeners */
 checkBox.addEventListener('click', handleCheck);
 start.addEventListener('click', gameOn);
+strict.addEventListener('click', strictMode);
